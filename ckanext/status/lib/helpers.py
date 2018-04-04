eba82d1dd3e1e5c8ddc4f842b30b7b07b097b6e1
Alice Butcher <alycejenni@gmail.com>
2018-04-04 15:02:59 +0100

    manual changes to lib

--- ckanext/status/lib/helpers.py
+++ ckanext/status/lib/helpers.py
@@ -4,17 +4,15 @@
 # This file is part of ckanext-status
 # Created by the Natural History Museum in London, UK
 
-import pylons
+from ckan.plugins import toolkit
 
 
 def status_get_message():
     '''Retrieve status message from config file
 
-
     :returns: status message specified in config file (or None if not specified)
-
     :rtype: string
 
     '''
 
-    return pylons.config.get(u'ckanext.status.message', None)
+    return toolkit.config.get(u'ckanext.status.message', None)
