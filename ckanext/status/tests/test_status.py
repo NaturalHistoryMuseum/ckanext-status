97d4a05d63fec0611563648c6825f1b58be0d57b
Alice Butcher <alycejenni@gmail.com>
2019-08-16 14:07:29 +0100

    Use ckantest for tests

--- ckanext/status/tests/test_status.py
+++ ckanext/status/tests/test_status.py
@@ -6,50 +6,29 @@
 
 import nose
 from ckanext.status.lib.helpers import status_get_message
-
-from ckan import plugins
-from ckan.plugins import toolkit
-from ckan.tests import helpers
-
-eq_ = nose.tools.eq_
-
-test_msg = u'this is a status message'
+from ckantest.models import TestBase
 
 
-class TestStatusMessage(object):
+class TestStatusMessage(TestBase):
     ''' '''
+    plugins = [u'status']
+    _test_msg = u'this is a status message'
 
-    @classmethod
-    def setup_class(cls):
-        ''' '''
-        cls.app = helpers._get_test_app()
-        if not plugins.plugin_loaded(u'status'):
-            plugins.load(u'status')
-
-    @classmethod
-    def teardown_class(cls):
-        ''' '''
-        plugins.unload(u'status')
-
-    def _set_msg(self, has_status_message):
-        '''
-
-        :param has_status_message: 
-
-        '''
-        if has_status_message:
-            toolkit.config[u'ckanext.status.message'] = test_msg
-        elif u'ckanext.status.message' in toolkit.config:
-            del toolkit.config[u'ckanext.status.message']
+    def tearDown(self):
+        self.config.soft_reset()
 
     def test_helper_gets_message_when_present(self):
         ''' '''
-        self._set_msg(has_status_message=True)
+        self.config.update({
+                               u'ckanext.status.message': self._test_msg
+                               })
         msg = status_get_message()
-        eq_(test_msg, msg)
+        nose.tools.assert_equal(self._test_msg, msg)
 
     def test_helper_gets_none_when_absent(self):
         ''' '''
-        self._set_msg(has_status_message=False)
+        # make sure it's not in the config
+        self.config.remove([u'ckanext.status.message'])
+        assert u'ckanext.status.message' not in self.config.current
         msg = status_get_message()
-        eq_(None, msg)
+        nose.tools.assert_equal(None, msg)
